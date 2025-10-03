61e007a59f08f0d3fa02c4f79abbafa6e9a7316d
Kishor Neupane <neupkishor.4@gmail.com>
2025-10-03 16:02:12 +0000

    use dashboard layout on the template creation page as well.

--- src/app/root/templates/[id]/edit/page.tsx
+++
@@ -1,9 +0,0 @@
-
-import { redirect } from 'next/navigation';
-
-export default function EditTemplateRedirectPage({ params }: { params: { id: string } }) {
-  redirect(`/root/templates/${params.id}/edit/basics`);
-  
-  // This will not be rendered as the user is redirected.
-  return null;
-}
