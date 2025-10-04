93f3275269053a376191863460c6f4cd8aa9915d
Kishor Neupane <neupkishor.4@gmail.com>
2025-10-04 09:54:39 +0000

    🔸 Current situation useEffect(() => {   NProgress.done(); }, [pathname,

--- src/components/ui/progress-bar.tsx
+++ src/components/ui/progress-bar.tsx
@@ -9,22 +9,12 @@ export function ProgressBar() {
   const searchParams = useSearchParams();
 
   useEffect(() => {
+    NProgress.configure({ showSpinner: false });
+  }, []);
+
+  useEffect(() => {
     NProgress.done();
   }, [pathname, searchParams]);
 
-  useEffect(() => {
-    // This effect should run only once on mount to set up the configuration
-    NProgress.configure({ showSpinner: false });
-
-    // This is the cleanup function that will be called when the component unmounts.
-    return () => {
-      // Although NProgress.done() is called on route changes,
-      // it's good practice to ensure it's cleaned up on unmount as well.
-      NProgress.done();
-    };
-  }, []);
-
-  // We are not rendering anything from this component. It's just for handling the side effects.
-  // A component can't return a `useEffect` directly. It must return a valid React node, `null` in this case.
   return null;
 }
