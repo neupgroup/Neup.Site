335072519b18ee4fb3308283054b3c10a716cb77
Kishor Neupane <neupkishor.4@gmail.com>
2025-10-04 16:30:24 +0000

    i don't know what the fuxk you did but we're getting the dashboard layou

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -2,17 +2,19 @@
 import { ProfileProvider } from '@/context/ProfileContext';
 import { ProgressBar } from '@/components/ui/progress-bar';
 import { Dashboard } from '@/components/dashboard';
+import { getSite } from '@/actions/editor/site';
 
 export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const { site } = await getSite();
 
   return (
     <ProfileProvider>
       <ProgressBar />
-      <Dashboard>{children}</Dashboard>
+      <Dashboard theme={site?.theme}>{children}</Dashboard>
     </ProfileProvider>
   );
 }
